2499c5927442a7fd0ff7f614a7bee152b04395c3
zaka0406 <zaka0406@gmail.com>
2023-04-21 08:14:20 +0900

    Render用に設定を再修正

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module InfoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'infoapp-user174.onrender.com'
   end
 end
